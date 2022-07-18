1b341da2bd17f5322513c0642ec9a5c0d4f67943
oussama <oussamaeessid@gmail.com>
2022-07-18 16:47:43 +0200

    commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+        return $request->user();
+    }
+
+    public function register(Request $request) {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $result= User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => bcrypt($request['password'])
+        ]);
+
+      //  $token = $user->createToken('myapptoken')->plainTextToken;
+
+      //  $response = [
+      //      'user' => $user,
+     //      'token' => $token
+      //  ];
+
+        return $result;
+    }
+
+    public function login(Request $request) {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        if(Auth::attempt($fields)){
+            $user = Auth::user();
+            $token = md5( time() ).'.'.md5($request->email);
+            $user->forceFill([
+                'api_token'=>$token,
+            ])->save();
+            return response()->json([
+                'token'=>$token
+            ]);
+
+        }
+
+        return response()->json([
+              'message'=>'the producted credentialsdo not much '  
+        ]);
+    
+        }
+
+        public function logout(Request $request) {
+            $request->user()->forceFill([
+                'api_token'=> null,
+            ])->save();
+
+            return response()->json(['message'=>'success' ]);
+        }
+}
---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.master')
+
+@section('title','HandyMan');
+
+@section('content')
+
+<div class="container-fluid px-4">
+                        <h1 class="mt-4">Dashboard</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">Dashboard</li>
+                        </ol>
+                        <div class="row">
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-primary text-white mb-4">
+                                <div class="card-body">0</div>
+                                    <div class="card-body">Total Booking</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View All</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-primary text-white mb-4">
+                                <div class="card-body">0</div>
+                                    <div class="card-body">Total Service</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View All</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-primary text-white mb-4">
+                                <div class="card-body">1</div>
+                                    <div class="card-body">Total Provider</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View All</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-primary text-white mb-4">
+                                <div class="card-body">0</div>
+                                    <div class="card-body">Total Revenue</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="#">View All</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                        </div>
+</div>
+
+@endsection
